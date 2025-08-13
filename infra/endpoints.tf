cfd261080cd96b9d72564e654deea052e7a0de3d
Aravind Koniki <aravindkoniki@AK-PT-MacBook-Pro.local>
2025-08-13 19:09:38 +0400

    added VPC endpoints

---
+++ infra/endpoints.tf
@@ -0,0 +1,202 @@
+# VPC Endpoints for ECR and S3 access
+# This allows EKS nodes to pull container images from ECR without internet access
+
+# Security group for VPC endpoints
+resource "aws_security_group" "vpc_endpoints" {
+  provider    = aws.MY_NETWORKING
+  name        = "${var.name}-vpc-endpoints"
+  description = "Security group for VPC endpoints"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  egress {
+    description = "All outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-vpc-endpoints"
+  }
+}
+
+# Interface VPC Endpoint for ECR API (control plane)
+resource "aws_vpc_endpoint" "ecr_api" {
+  provider            = aws.MY_NETWORKING
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.region}.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.vpc.private_subnet_ids
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  private_dns_enabled = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:DescribeRepositories",
+          "ecr:DescribeImages",
+          "ecr:ListImages"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.name}-ecr-api-endpoint"
+  }
+}
+
+# Interface VPC Endpoint for ECR DKR (data plane - image layers)
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  provider            = aws.MY_NETWORKING
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.region}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.vpc.private_subnet_ids
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  private_dns_enabled = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.name}-ecr-dkr-endpoint"
+  }
+}
+
+# Gateway VPC Endpoint for S3 (ECR stores image layers in S3)
+resource "aws_vpc_endpoint" "s3" {
+  provider          = aws.MY_NETWORKING
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = module.vpc.private_route_table_ids
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:GetObjectVersion",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "*"
+        ]
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.name}-s3-endpoint"
+  }
+}
+
+# Optional: VPC Endpoint for CloudWatch Logs (if you want to send logs without internet)
+resource "aws_vpc_endpoint" "logs" {
+  provider            = aws.MY_NETWORKING
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.region}.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.vpc.private_subnet_ids
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  private_dns_enabled = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.name}-logs-endpoint"
+  }
+}
+
+# Optional: VPC Endpoint for STS (Security Token Service)
+resource "aws_vpc_endpoint" "sts" {
+  provider            = aws.MY_NETWORKING
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.region}.sts"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.vpc.private_subnet_ids
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  private_dns_enabled = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "sts:AssumeRole",
+          "sts:AssumeRoleWithWebIdentity",
+          "sts:GetCallerIdentity"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.name}-sts-endpoint"
+  }
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,3 +5,29 @@ output "node_role_arn" {
 output "bastion_public_ip" {
   value = var.endpoint_public_access == false ? aws_instance.bastion[0].public_ip : null
 }
+
+output "vpc_endpoints" {
+  description = "VPC Endpoints for private ECR and S3 access"
+  value = {
+    ecr_api = aws_vpc_endpoint.ecr_api.id
+    ecr_dkr = aws_vpc_endpoint.ecr_dkr.id
+    s3      = aws_vpc_endpoint.s3.id
+    logs    = aws_vpc_endpoint.logs.id
+    sts     = aws_vpc_endpoint.sts.id
+  }
+}
+
+output "vpc_endpoints_dns_entries" {
+  description = "DNS entries for VPC endpoints"
+  value = {
+    ecr_api = aws_vpc_endpoint.ecr_api.dns_entry
+    ecr_dkr = aws_vpc_endpoint.ecr_dkr.dns_entry
+    logs    = aws_vpc_endpoint.logs.dns_entry
+    sts     = aws_vpc_endpoint.sts.dns_entry
+  }
+}
+ 
+output "vpc_endpoints_security_group_id" {
+  description = "Security group ID for VPC endpoints"
+  value       = aws_security_group.vpc_endpoints.id
+}
