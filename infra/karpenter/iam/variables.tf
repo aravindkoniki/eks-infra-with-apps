684e668aafa336af71f5adb05cb1309e0cc2a65c
Aravind Koniki <aravind_koniki@hotmail.com>
2025-09-06 16:09:19 +0400

    Working on karpenter module

--- infra/karpenter/iam/variables.tf
+++ infra/karpenter/iam/variables.tf
@@ -3,22 +3,11 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "region" {
-  description = "AWS region"
-  type        = string
-}
-
 variable "oidc_provider_arn" {
   description = "OIDC provider ARN for cluster (for IRSA assume role)"
   type        = string
 }
 
-
-variable "karpenter_version" {
-  description = "Karpenter Helm chart version (or chart tag)"
-  type        = string
-  default     = "v0.33.0" # adjust as needed
-}
 
 variable "tags" {
   description = "Tags to attach to IAM roles / instance profile"
